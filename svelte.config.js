c34d17aa3971550edfc519008ccd8fba11e4516c
digiot <digiot.abc@gmail.com>
2026-01-13 13:45:50 +0900

    initial commit.

--- svelte.config.js
+++ svelte.config.js
@@ -19,7 +19,7 @@ const config = {
       strict: true
     }),
     paths: {
-      base: isProdPages ? '/wellbeing-room' : ''
+      base: isProdPages ? '/sticky-form' : ''
     }
     // paths: {
     // 	base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
