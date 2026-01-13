ac4d09d9feec55d1141dbc8661018cff027d15ce
digiot <digiot.abc@gmail.com>
2026-01-13 14:26:40 +0900

    initial commit.

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,23 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import {vitePreprocess} from '@sveltejs/vite-plugin-svelte';
+
+const isProdPages = process.env.DEPLOY_TARGET === 'gh-pages';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
-
   kit: {
     adapter: adapter({
+      // default options are shown. On some platforms
+      // these options are set automatically — see below
       pages: 'build',
       assets: 'build',
-      fallback: 'index.html'
+      fallback: 'index.html',
+      precompress: false,
+      strict: true
     }),
-
     paths: {
-      base: ''
+      base: isProdPages ? '/sticky-form' : ''
     }
   }
 };
